82b1dad2d3d742981362489bd779437fed39ae4f
chundo <segundo.espana1@gmail.com>
2019-07-16 12:43:33 -0500

    Routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  resources :permissions
-  resources :parameters
-  resources :cargapp_models
-  resources :user_roles
   get 'home/index'
   get 'home/user_information'
   
   devise_for :users
   resources :roles
+  resources :user_roles
+  resources :cargapp_models
+  resources :parameters
+  resources :permissions
 
   namespace :api, defaults: { format: :json } do
     namespace :v1, defaults: { format: :json } do
       get 'roles/active' => 'roles#active'
       resources :roles
       resources :users#, only: [:create, :update, :show]
+      resources :user_roles
+      resources :cargapp_models
+      resources :parameters
+      resources :permissions
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
