db6ca474f338a1eb5730d44d1369521a9bc1ea88
chundo <segundo.espana1@gmail.com>
2019-08-11 14:44:10 -0500

    Error deploy

--- app/models/user_coupon.rb
+++ app/models/user_coupon.rb
@@ -2,4 +2,6 @@ class UserCoupon < ApplicationRecord
   belongs_to :cargapp_model
   belongs_to :coupon
   belongs_to :user
+  validates :user_id, uniqueness: { scope: [:coupon_id] }
+  validates :applied_item_id, uniqueness: { scope: [:cargapp_model_id, :user_id] }
 end
