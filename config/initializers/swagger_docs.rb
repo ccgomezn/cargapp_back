63c1ce084379586c86f5d751a46bbb4fc8ba57cf
chundo <segundo.espana1@gmail.com>
2019-08-13 16:50:20 -0500

    Swagger

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -4,7 +4,7 @@ Swagger::Docs::Config.register_apis({
     # the output location where your .json files are written to
     :api_file_path => "public/",
     # the URL base path to your API
-    :base_path => "http://localhost:3000",
+    :base_path => ENV['URL'],
     # if you want to delete all .json files at each generation
     :clean_directory => false,
     # Ability to setup base controller for each api version. Api::V1::SomeController for example.
