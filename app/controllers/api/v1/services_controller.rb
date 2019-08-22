14eae64d27eaf633c85605db729cc24274b23636
chundo <segundo.espana1@gmail.com>
2019-08-22 10:45:44 -0500

    Service

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -7,60 +7,49 @@ class Api::V1::ServicesController < ApplicationController
   # GET /services.json
   def index
     @services = Service.all
+    render json: @services
   end
 
+  def active
+    @services = Service.where(active: true)
+    render json: @services
+  end
+  
+  def me
+    @services = @user.services
+    render json: @services
+  end
   # GET /services/1
   # GET /services/1.json
   def show
-  end
-
-  # GET /services/new
-  def new
-    @service = Service.new
-  end
-
-  # GET /services/1/edit
-  def edit
+    render json: @service
   end
 
   # POST /services
   # POST /services.json
   def create
     @service = Service.new(service_params)
-
-    respond_to do |format|
-      if @service.save
-        format.html { redirect_to @service, notice: 'Service was successfully created.' }
-        format.json { render :show, status: :created, location: @service }
-      else
-        format.html { render :new }
-        format.json { render json: @service.errors, status: :unprocessable_entity }
-      end
+    if @service.save
+      render json: service, status: :created, location: @service
+    else
+      render json: @service.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /services/1
   # PATCH/PUT /services/1.json
   def update
-    respond_to do |format|
       if @service.update(service_params)
-        format.html { redirect_to @service, notice: 'Service was successfully updated.' }
-        format.json { render :show, status: :ok, location: @service }
+        render json: @service, status: :ok, location: @service
       else
-        format.html { render :edit }
-        format.json { render json: @service.errors, status: :unprocessable_entity }
+        render json: @service.errors, status: :unprocessable_entity
       end
-    end
   end
 
   # DELETE /services/1
   # DELETE /services/1.json
   def destroy
     @service.destroy
-    respond_to do |format|
-      format.html { redirect_to services_url, notice: 'Service was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
