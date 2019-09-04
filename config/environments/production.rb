a48db5266daeffc9656378a5918329455f996dc4
chundo <segundo.espana1@gmail.com>
2019-09-04 11:42:35 -0500

    Email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  #config.action_mailer.default_url_options = { host: 'api.cargapp.co', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: ENV['URL'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['MAILER_POWER_ADDRESS'],
+    port:                 ENV['MAILER_POWER_PORT'],
+    domain:               ENV['MAILER_POWER_DOMAIN'],
+    user_name:            ENV['MAILER_POWER_USER_NAME'],
+    password:             ENV['MAILER_POWER_PASSWORD'],
+    authentication:       ENV['MAILER_POWER_AUT'],
+    enable_starttls_auto: true }
+end
 end
