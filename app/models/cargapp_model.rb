e099419336396ffb63ea4c66f7e050f4c3ca0090
chundo <segundo.espana1@gmail.com>
2019-11-15 10:40:45 -0500

    Error status

--- app/models/cargapp_model.rb
+++ app/models/cargapp_model.rb
@@ -3,5 +3,5 @@
 class CargappModel < ApplicationRecord
   validates :name, :code, :value, uniqueness: true
   validates :name, :code, :value, presence: true
-  has_many :status
+  #has_many :status
 end
