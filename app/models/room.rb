111e512189c33de45a9394e4da4d6fc440752d36
chundo <segundo.espana1@gmail.com>
2019-12-22 12:08:05 -0500

    destroy room users in room

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,5 @@ class Room < ApplicationRecord
   belongs_to :service, optional: true
   belongs_to :user
   validates :service_id, uniqueness: { scope: [:user_id] }
+  has_many :room_users, dependent: :destroy #P
 end
