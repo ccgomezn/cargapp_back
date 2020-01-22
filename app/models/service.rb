48882f0ba9deff60cb28f161c9ae14bf15560bb6
chundo <segundo.espana1@gmail.com>
2020-01-22 12:13:32 -0500

    update default

--- app/models/service.rb
+++ app/models/service.rb
@@ -15,6 +15,7 @@ class Service < ApplicationRecord
   has_many :rate_services, dependent: :destroy #P
 
   before_create :default_values
+  before_save :default_values
   def default_values
     self.origin = self.origin.capitalize
     self.destination = self.destination.capitalize
