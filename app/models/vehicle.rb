559a4b60381a4849a80d3a4820a93773af1fb115
chundo <segundo.espana1@gmail.com>
2020-03-06 11:13:53 -0500

    destroy vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class Vehicle < ApplicationRecord
   belongs_to :vehicle_type
-  belongs_to :owner_document_type, :class_name => 'DocumentType', optional: true
+  belongs_to :owner_document_type, class_name: 'DocumentType', optional: true
   belongs_to :user
+  has_many :vehicle_documents, dependent: :destroy
 end
